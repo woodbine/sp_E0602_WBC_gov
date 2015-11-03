0f5484f5c4c36e9ad58bb95e95c87f9a8994ec8b
blablupcom <olegvolodin@eyahoo.com>
2015-11-03 20:08:25 +0300

    first commit

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
 import requests
-from dateutil.parser import parse
+
 
 #### FUNCTIONS 1.0
 
@@ -38,20 +38,39 @@ def validateFilename(filename):
 
 
 def validateURL(url):
+    # try:
+    #     r = requests.get(url, allow_redirects=True, timeout=20)
+    #     count = 1
+    #     while r.status_code == 500 and count < 4:
+    #         print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
+    #         count += 1
+    #         r = requests.get(url, allow_redirects=True, timeout=20)
+    #     sourceFilename = r.headers.get('Content-Disposition')
+    #     if sourceFilename:
+    #         ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
+    #     else:
+    #         ext = os.path.splitext(url)[1]
+    #     validURL = r.status_code == 200
+    #     validFiletype = ext in ['.csv', '.xls', '.xlsx']
+    #     return validURL, validFiletype
+    # except:
+    #     print ("Error validating URL.")
+    #     return False, False
     try:
-        r = requests.get(url, allow_redirects=True, timeout=20)
+        r = urllib2.urlopen(url)
         count = 1
-        while r.status_code == 500 and count < 4:
+        while r.getcode() == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            r = requests.get(url, allow_redirects=True, timeout=20)
+            r = urllib2.urlopen(url)
         sourceFilename = r.headers.get('Content-Disposition')
+
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
         else:
             ext = os.path.splitext(url)[1]
-        validURL = r.status_code == 200
-        validFiletype = ext in ['.csv', '.xls', '.xlsx']
+        validURL = r.getcode() == 200
+        validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
         return validURL, validFiletype
     except:
         print ("Error validating URL.")
@@ -109,7 +128,7 @@ for link in links:
         csvfile = link.text.split()
         if len(csvfile) == 4:
             csvYr =csvfile[-1]
-            csvMth = csvfile[-2].strip()[:3]
+            csvMth = 'Q0'
         if len(csvfile) == 2:
             csvYr =csvfile[-1].strip()
             csvMth = csvfile[0].strip()[:3]
